ebbd447cd41b04af4f25b25ecd316b0817152d22
Akaalroop Singh <akaal@akaalroop.com>
2025-09-13 20:03:23 +0100

    Corrected the spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,12 @@
 <div class="@container mx-auto p-4 text-center text-black flex flex-col">
 <h1 class="text-4xl md:text-5xl">Welcome to Reds!</h1>
 <h2 class="text-2xl md:text-3xl my-4">Please select a button below</h2>
-	<div class="@container/buttons gap-y- w-full">
-		<div class="@container/4-cards flex flex-col mx-auto items-center mb-5">
+	<div class="@container/buttons space-y-6 w-full">
+		<div class="@container/4-cards flex flex-col mx-auto items-center">
 		<button id="4-cards" class="bg-blue py-14 rounded-2xl text-2xl md:text-3xl my-2 hover:scale-[1.03] hover:-translate-y-2 hover:shadow-2xl min-w-[80%]">4 cards</button>
 			<p class="text-xl md:text-2xl">You will be able to look at the first <b>2</b> cards</p>
 		</div>
-		<div class="@container/4-cards flex flex-col mx-auto items-center mb-5">
+		<div class="@container/4-cards flex flex-col mx-auto items-center">
 			<button id="4-cards" class="bg-green py-14 rounded-2xl text-2xl md:text-3xl my-2 hover:scale-[1.03] hover:-translate-y-2 hover:shadow-2xl min-w-[80%]">6 cards</button>
 			<p class="text-xl md:text-2xl">You will be able to look at the first <b>3</b> cards</p>
 		</div>
