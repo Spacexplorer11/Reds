8ab3ddbccbf1e8782a71ebc7dbf2a577b3c0a99d
Akaalroop Singh <akaal@akaalroop.com>
2025-09-13 19:24:54 +0100

    Changed font to Young Serif

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to Reds!</h1>
-<p>This website is currently in progress!</p>
+<script>
+</script>
+
+<div class="@container mx-auto p-4 text-center text-black">
+<h1 class="text-4xl md:text-5xl">Welcome to Reds!</h1>
+<h2 class="text-2xl md:text-3xl mt-4">Please select a button below</h2>
+</div>
