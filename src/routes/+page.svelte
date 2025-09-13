243a0ce21907d54861d84034dd77b1da69b99bbf
Akaalroop Singh <akaal@akaalroop.com>
2025-09-13 19:57:10 +0100

    Add 6 card button - REDS-3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,13 @@
 <h1 class="text-4xl md:text-5xl">Welcome to Reds!</h1>
 <h2 class="text-2xl md:text-3xl my-4">Please select a button below</h2>
 	<div class="@container/buttons gap-y- w-full">
-		<div class="@container/4-cards flex flex-col mx-auto items-center">
+		<div class="@container/4-cards flex flex-col mx-auto items-center mb-5">
 		<button id="4-cards" class="bg-blue py-14 rounded-2xl text-2xl md:text-3xl my-2 hover:scale-[1.03] hover:-translate-y-2 hover:shadow-2xl min-w-[80%]">4 cards</button>
-			<p class="text-xl md:text-2xl">You will be able to look at the first 2 cards</p>
+			<p class="text-xl md:text-2xl">You will be able to look at the first <b>2</b> cards</p>
+		</div>
+		<div class="@container/4-cards flex flex-col mx-auto items-center mb-5">
+			<button id="4-cards" class="bg-green py-14 rounded-2xl text-2xl md:text-3xl my-2 hover:scale-[1.03] hover:-translate-y-2 hover:shadow-2xl min-w-[80%]">6 cards</button>
+			<p class="text-xl md:text-2xl">You will be able to look at the first <b>3</b> cards</p>
 		</div>
 	</div>
 </div>
