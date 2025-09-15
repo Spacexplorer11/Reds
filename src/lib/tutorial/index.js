39e7abd65916c21e37739c47d565cd7b44740dba
Ferboinkles <vihaan4nasa@gmail.com>
2025-09-15 17:26:55 +0100

    Added step 1 and 2 - REDS-13
    Signed-off-by: Ferboinkles <vihaan4nasa@gmail.com>

--- src/lib/tutorial/index.js
+++ src/lib/tutorial/index.js
@@ -1,6 +1,6 @@
 // Edit this to make sure the frontend can access the tutorial steps
-// TODO: @ferboinkles write more steps and link them here
 //import step1 from './example.json';
 import step1 from './1.json';
-
-export default [step1];
+import step2 from './2.json';
+import step3 from './3.json';
+export default [step1, step2, step3];
