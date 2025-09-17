9f010fd52d509692dbb0d64092d873e31452dfb0
Ferboinkles <vihaan4nasa@gmail.com>
2025-09-17 18:25:56 +0100

    Added step 5 - REDS-13
    Signed-off-by: Ferboinkles <vihaan4nasa@gmail.com>

--- src/lib/tutorial/index.js
+++ src/lib/tutorial/index.js
@@ -4,4 +4,5 @@ import step1 from './1.json';
 import step2 from './2.json';
 import step3 from './3.json';
 import step4 from './4.json';
-export default [step1, step2, step3, step4];
+import step5 from './5.json';
+export default [step1, step2, step3, step4, step5];
