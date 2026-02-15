b22185df9b4fc4ffffa5ebb6cb0455f22942a1ef
Akaalroop Singh <akaal@akaalroop.com>
2026-02-15 19:15:49 +0000

    Added the cards at right!
    Signed-off-by: Akaalroop Singh <akaal@akaalroop.com>

--- src/routes/(game)/6-card/+page.svelte
+++ src/routes/(game)/6-card/+page.svelte
@@ -20,25 +20,46 @@
 		<img alt="back of a card" src="/images/cards/cardback.webp" />
 	</button>
 </div>
+<div class="@container/middle grid grid-cols-2">
+	<div class="@container/left grid max-w-[50%] rotate-90 grid-cols-3 gap-1.25">
+		<button>
+			<img alt="back of a card" src="/images/cards/cardback.webp" />
+		</button>
+		<button>
+			<img alt="back of a card" src="/images/cards/cardback.webp" />
+		</button>
+		<button>
+			<img alt="back of a card" src="/images/cards/cardback.webp" />
+		</button>
+		<button>
+			<img alt="back of a card" src="/images/cards/cardback.webp" />
+		</button><button>
+			<img alt="back of a card" src="/images/cards/cardback.webp" />
+		</button>
+		<button>
+			<img alt="back of a card" src="/images/cards/cardback.webp" />
+		</button>
+	</div>
 
-<div class="@container/left grid max-w-[30%] rotate-90 grid-cols-3 gap-1.25">
-	<button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button>
-	<button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button>
-	<button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button>
-	<button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button><button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button>
-	<button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button>
+	<div class="@container/right ml-[50%] grid max-w-[50%] rotate-270 grid-cols-3 gap-1.25">
+		<button>
+			<img alt="back of a card" src="/images/cards/cardback.webp" />
+		</button>
+		<button>
+			<img alt="back of a card" src="/images/cards/cardback.webp" />
+		</button>
+		<button>
+			<img alt="back of a card" src="/images/cards/cardback.webp" />
+		</button>
+		<button>
+			<img alt="back of a card" src="/images/cards/cardback.webp" />
+		</button><button>
+			<img alt="back of a card" src="/images/cards/cardback.webp" />
+		</button>
+		<button>
+			<img alt="back of a card" src="/images/cards/cardback.webp" />
+		</button>
+	</div>
 </div>
 
 <div class="@container/bottom mt-[15vh] ml-[35%] grid max-w-[30%] grid-cols-3 gap-1.25">
