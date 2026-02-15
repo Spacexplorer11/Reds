77efd0334580ca7064a8f04ba251240877c78faf
Akaalroop Singh <akaal@akaalroop.com>
2026-02-15 18:57:44 +0000

    Added the cards at bottom
    Signed-off-by: Akaalroop Singh <akaal@akaalroop.com>

--- src/routes/(game)/6-card/+page.svelte
+++ src/routes/(game)/6-card/+page.svelte
@@ -20,3 +20,23 @@
 		<img alt="back of a card" src="/images/cards/cardback.webp" />
 	</button>
 </div>
+
+<div class="@container/bottom mt-[15vh] ml-[35%] grid max-w-[30%] grid-cols-3 gap-1.25">
+	<button>
+		<img alt="back of a card" src="/images/cards/cardback.webp" />
+	</button>
+	<button>
+		<img alt="back of a card" src="/images/cards/cardback.webp" />
+	</button>
+	<button>
+		<img alt="back of a card" src="/images/cards/cardback.webp" />
+	</button>
+	<button>
+		<img alt="back of a card" src="/images/cards/cardback.webp" />
+	</button><button>
+		<img alt="back of a card" src="/images/cards/cardback.webp" />
+	</button>
+	<button>
+		<img alt="back of a card" src="/images/cards/cardback.webp" />
+	</button>
+</div>
