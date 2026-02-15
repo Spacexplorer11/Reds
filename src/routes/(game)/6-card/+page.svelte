03140ef45d3476abf57a4ca2b2227dbaf1f8615a
Akaalroop Singh <akaal@akaalroop.com>
2026-02-15 19:03:26 +0000

    Added the cards at left
    Signed-off-by: Akaalroop Singh <akaal@akaalroop.com>

--- src/routes/(game)/6-card/+page.svelte
+++ src/routes/(game)/6-card/+page.svelte
@@ -1,7 +1,27 @@
 <script>
 </script>
 
-<div class="@container/top ml-[35%] grid max-w-[30%] grid-cols-3 gap-1.25">
+<div class="@container/top mb-[15vh] ml-[35%] grid max-w-[30%] grid-cols-3 gap-1.25">
+	<button>
+		<img alt="back of a card" src="/images/cards/cardback.webp" />
+	</button>
+	<button>
+		<img alt="back of a card" src="/images/cards/cardback.webp" />
+	</button>
+	<button>
+		<img alt="back of a card" src="/images/cards/cardback.webp" />
+	</button>
+	<button>
+		<img alt="back of a card" src="/images/cards/cardback.webp" />
+	</button><button>
+		<img alt="back of a card" src="/images/cards/cardback.webp" />
+	</button>
+	<button>
+		<img alt="back of a card" src="/images/cards/cardback.webp" />
+	</button>
+</div>
+
+<div class="@container/left grid max-w-[30%] rotate-90 grid-cols-3 gap-1.25">
 	<button>
 		<img alt="back of a card" src="/images/cards/cardback.webp" />
 	</button>
