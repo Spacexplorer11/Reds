bf5743b817794184d088ad3b0dcfbdd1459fd8cf
Akaalroop Singh <akaal@akaalroop.com>
2026-02-15 19:23:09 +0000

    Implement @coderabbitai's suggestion and make the code DRY
    Signed-off-by: Akaalroop Singh <akaal@akaalroop.com>

--- src/routes/(game)/6-card/+page.svelte
+++ src/routes/(game)/6-card/+page.svelte
@@ -1,83 +1,24 @@
-<script>
-</script>
+{#snippet cardGroup()}
+	{#each Array(6) as _}
+		<button type="button">
+			<img alt="back of a card" src="/images/cards/cardback.webp" />
+		</button>
+	{/each}
+{/snippet}
 
 <div class="@container/top mb-[15vh] ml-[35%] grid max-w-[30%] grid-cols-3 gap-1.25">
-	<button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button>
-	<button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button>
-	<button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button>
-	<button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button><button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button>
-	<button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button>
+	{@render cardGroup()}
 </div>
+
 <div class="@container/middle grid grid-cols-2">
 	<div class="@container/left grid max-w-[50%] rotate-90 grid-cols-3 gap-1.25">
-		<button>
-			<img alt="back of a card" src="/images/cards/cardback.webp" />
-		</button>
-		<button>
-			<img alt="back of a card" src="/images/cards/cardback.webp" />
-		</button>
-		<button>
-			<img alt="back of a card" src="/images/cards/cardback.webp" />
-		</button>
-		<button>
-			<img alt="back of a card" src="/images/cards/cardback.webp" />
-		</button><button>
-			<img alt="back of a card" src="/images/cards/cardback.webp" />
-		</button>
-		<button>
-			<img alt="back of a card" src="/images/cards/cardback.webp" />
-		</button>
+		{@render cardGroup()}
 	</div>
-
 	<div class="@container/right ml-[50%] grid max-w-[50%] rotate-270 grid-cols-3 gap-1.25">
-		<button>
-			<img alt="back of a card" src="/images/cards/cardback.webp" />
-		</button>
-		<button>
-			<img alt="back of a card" src="/images/cards/cardback.webp" />
-		</button>
-		<button>
-			<img alt="back of a card" src="/images/cards/cardback.webp" />
-		</button>
-		<button>
-			<img alt="back of a card" src="/images/cards/cardback.webp" />
-		</button><button>
-			<img alt="back of a card" src="/images/cards/cardback.webp" />
-		</button>
-		<button>
-			<img alt="back of a card" src="/images/cards/cardback.webp" />
-		</button>
+		{@render cardGroup()}
 	</div>
 </div>
 
 <div class="@container/bottom mt-[15vh] ml-[35%] grid max-w-[30%] grid-cols-3 gap-1.25">
-	<button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button>
-	<button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button>
-	<button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button>
-	<button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button><button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button>
-	<button>
-		<img alt="back of a card" src="/images/cards/cardback.webp" />
-	</button>
+	{@render cardGroup()}
 </div>
