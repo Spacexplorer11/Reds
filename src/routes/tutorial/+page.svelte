509fff7f0ddb91a564037ef04accba39d499a477
Akaalroop Singh <akaal@akaalroop.com>
2026-02-17 16:44:05 +0000

    Make the font sizes more responsive and different for each tailwind breakpoint
    Signed-off-by: Akaalroop Singh <akaal@akaalroop.com>

--- src/routes/tutorial/+page.svelte
+++ src/routes/tutorial/+page.svelte
@@ -7,14 +7,12 @@
 
 <div class="@container mx-auto flex flex-col p-4 text-center text-black">
 	<a href="/" class="-top-2.5 left-0.5 max-w-fit"><House /></a>
-	<h1 class="text-4xl md:text-5xl">Tutorial</h1>
-	<h2 class="my-4 text-2xl md:text-3xl">Learn how to play Reds!</h2>
+	<h1 class="text-3xl md:text-4xl lg:text-5xl">Tutorial</h1>
+	<h2 class="my-4 text-xl md:text-2xl lg:text-3xl">Learn how to play Reds!</h2>
 	<main class="@container/main gap-10">
-		<div
-			class="@container/step mx-auto flex w-3/5 flex-col items-center rounded-2xl bg-salmon p-10"
-		>
-			<h3 class="my-2 text-2xl whitespace-pre-wrap md:text-3xl">{stepJSON.title}</h3>
-			<p class="my-4 text-xl whitespace-pre-wrap md:text-2xl">
+		<div class="@container/step mr-2 ml-2 flex flex-col items-center rounded-2xl bg-salmon p-10">
+			<h3 class="my-2 text-xl whitespace-pre-wrap md:text-2xl lg:text-3xl">{stepJSON.title}</h3>
+			<p class="text-l my-4 whitespace-pre-wrap md:text-xl lg:text-2xl">
 				{stepJSON.content}
 			</p>
 		</div>
