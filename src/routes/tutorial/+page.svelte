e2f958c78d49d7728b1052772664eedddae6fdd3
Akaalroop Singh <akaal@akaalroop.com>
2026-02-17 16:56:42 +0000

    Fix the tailwind classes' issues raised by @coderabbitai
    Signed-off-by: Akaalroop Singh <akaal@akaalroop.com>

--- src/routes/tutorial/+page.svelte
+++ src/routes/tutorial/+page.svelte
@@ -9,10 +9,10 @@
 	<a href="/" class="-top-2.5 left-0.5 max-w-fit"><House /></a>
 	<h1 class="text-3xl md:text-4xl lg:text-5xl">Tutorial</h1>
 	<h2 class="my-4 text-xl md:text-2xl lg:text-3xl">Learn how to play Reds!</h2>
-	<main class="@container/main gap-10">
+	<main class="@container/main">
 		<div class="@container/step mr-2 ml-2 flex flex-col items-center rounded-2xl bg-salmon p-10">
 			<h3 class="my-2 text-xl whitespace-pre-wrap md:text-2xl lg:text-3xl">{stepJSON.title}</h3>
-			<p class="text-l my-4 whitespace-pre-wrap md:text-xl lg:text-2xl">
+			<p class="my-4 text-lg whitespace-pre-wrap md:text-xl lg:text-2xl">
 				{stepJSON.content}
 			</p>
 		</div>
@@ -27,7 +27,7 @@
 				<ArrowBigLeft size="30" />
 			</button>
 			<button
-				class="max-width-fit max-h-fit rounded-2xl bg-blue p-5 disabled:cursor-not-allowed disabled:bg-gray-700"
+				class="max-h-fit max-w-fit rounded-2xl bg-blue p-5 disabled:cursor-not-allowed disabled:bg-gray-700"
 				onclick={() => {
 					if (step < steps.length) step += 1;
 				}}
