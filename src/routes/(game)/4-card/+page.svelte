50f20d8c62d38f26155ac9665c1c7f86d39b010b
ferboinkles <ferboinkles@gmail.com>
2026-02-17 21:41:14 +0000

    Fixed the problemo!

--- src/routes/(game)/4-card/+page.svelte
+++ src/routes/(game)/4-card/+page.svelte
@@ -6,19 +6,19 @@
 	{/each}
 {/snippet}
 
-<div class="@container/top mb-[15vh] ml-[25%] grid max-w-[20%] grid-cols-2 gap-1.25">
+<div class="@container/top mb-[15vh] ml-[40%] grid max-w-[20%] grid-cols-2 gap-1.25">
 	{@render cardGroup()}
 </div>
 
 <div class="@container/middle grid grid-cols-2">
-	<div class="@container/left grid max-w-[50%] rotate-90 grid-cols-2 gap-1.25">
+	<div class="@container/left ml-[10%] grid max-w-[40%] rotate-90 grid-cols-2 gap-1.25">
 		{@render cardGroup()}
 	</div>
-	<div class="@container/right ml-[50%] grid max-w-[50%] -rotate-90 grid-cols-2 gap-1.25">
+	<div class="@container/right ml-[50%] grid max-w-[40%] -rotate-90 grid-cols-2 gap-1.25">
 		{@render cardGroup()}
 	</div>
 </div>
 
-<div class="@container/bottom mt-[15vh] ml-[25%] grid max-w-[20%] grid-cols-2 gap-1.25">
+<div class="@container/bottom mt-[15vh] ml-[40%] grid max-w-[20%] grid-cols-2 gap-1.25">
 	{@render cardGroup()}
 </div>
